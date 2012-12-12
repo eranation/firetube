5f3b0b9228b255199877370423e3d38f4dcf55c9
startupandrew <startupandrew@gmail.com>
2012-12-12 15:17:29 -0800

    Added mustache templating.

--- firetube.js
+++ firetube.js
@@ -2,18 +2,13 @@ var commentsRef = new Firebase('https://firetube.firebaseio.com/');
 
 commentsRef.on('child_added', function (snapshot) {
   var comment = snapshot.val();
-  var graphBase = "https://graph.facebook.com/" + comment.userid
   $.ajax({
-    url: graphBase + "?fields=name",
+    url: "https://graph.facebook.com/" + comment.userid + "?fields=name",
     dataType: 'jsonp',
     success: function(userdata) {
-      $('<div/>')
-        .text(comment.body)
-        .addClass('comment')
-        .prepend($('<br/>'))
-        .prepend($('<span/>').text(userdata.name))
-        .prepend($('<img src="' + graphBase + '/picture">'))
-        .appendTo($('#commentsDiv'));
+      comment.name = userdata.name;
+      var template = $('#template').html();
+      $("#comments").append($(Mustache.to_html(template, comment)));
     }
   });
 });
