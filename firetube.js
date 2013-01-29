01eff9078361b5ca73093b143879c65d1cfd03c5
Andrew Lee <andrew@firebase.com>
2013-01-28 23:05:28 -0800

    Updated auth API to new version. Updated rules to be a little cleaner. Added a limit(10) so the number of comments doesn't get too long.

--- firetube.js
+++ firetube.js
@@ -1,8 +1,19 @@
 var commentsRef = new Firebase('https://firetube.firebaseio.com/comments');
 var myUserID = null;
 
+//Create an Firebase Simple Login client so we can do Facebook auth
+var authClient = new FirebaseAuthClient(commentsRef, function(error, user) {
+  if(user) {
+    myUserID = user.id;
+    $("#loginDiv").text(user.first_name + " " + user.last_name);
+  }
+});
+
+//Create a query for only the last 10 comments
+var last10Comments = commentsRef.limit(10);
+
 //Render Comments
-commentsRef.on('child_added', function (snapshot) {
+last10Comments.on('child_added', function (snapshot) {
   var comment = snapshot.val();
   var newDiv = $("<div/>").addClass("comment").attr("id",snapshot.name()).appendTo("#comments");
   FB.api("/" + comment.userid, function(userdata) {
@@ -25,15 +36,11 @@ function onCommentKeyDown(event) {
 }
 
 //Remove deleted comments
-commentsRef.on("child_removed", function(snapshot) {
+last10Comments.on("child_removed", function(snapshot) {
   $("#" + snapshot.name()).remove();
 });
 
 //Handle Login
 function onLoginButtonClicked() {
-  var authClient = new FirebaseAuthClient(commentsRef);
-  authClient.login("facebook", function(err, token, userInfo) {
-    myUserID = userInfo.id;
-    $("#loginDiv").text(userInfo.first_name + " " + userInfo.last_name);
-  });
+  authClient.login("facebook");
 }
